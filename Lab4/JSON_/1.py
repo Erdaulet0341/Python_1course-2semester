07ed75759300645e3bc4e6ac3cb6c10a092ac0a3
Erdaulet <erdaulet0341@gmail.com>
2022-03-02 21:49:37 +0600

    main

--- Lab4/JSON_/1.py
+++ Lab4/JSON_/1.py
@@ -1,23 +1,23 @@
 import json
-from textwrap import indent
 
 with open("Sample-data.json", "r") as json_file:
     json_ = json.load(json_file)
     
 print("\n")    
 print("Interface status")
-print('==========================================================================')
-print("DN                                          Discription     Speed      MTU")          
-print('----------------------------------------    --------------  ---------  ----')
+print('='*70)
+print("DN                                           Discription     Speed      MTU")          
+print('-------------------------------------------  --------------  ------     ----')
 
 imdata = json_["imdata"]
-cnt = 0
-
 for i in range(len(imdata)):
     for j in imdata[i]:
         for k in imdata[i][j]:
             speed= imdata[i][j][k]['speed']
             mtu = imdata[i][j][k]['mtu']
-            print(F"topology/pod-1/node-201/sys/phys-[eth1/{cnt}]                  {speed}     {mtu}")
-            cnt+=1
+            dn = imdata[i][j][k]['dn']
+            if dn.find('33')>0 or dn.find('34')>0 or dn.find('35')>0:
+                print(f'{dn}                   {speed}    {mtu}')
+            
+            
         
